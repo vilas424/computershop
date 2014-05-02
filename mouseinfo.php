577d789dd26f6987914bdfb174bc0df85eba3512
vilas424 <vilasbalaganur@gmail.com>
2014-05-02 23:28:55 +0530

    computershop

---
+++ mouseinfo.php
@@ -0,0 +1,184 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
+"http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<title>Computer Store</title>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">
+<link rel="stylesheet" type="text/css" href="style.css" />
+</head>
+<body>
+<form method="GET" action="laptopinfo.php">
+			<table width="996"  border="0" cellspacing="0" cellpadding="0" align="center">
+			  <tr>
+				<td height="607" valign="top">
+							<table width="996"  border="0" cellspacing="0" cellpadding="0">
+							  <tr>
+								<td width="285" height="52"><marquee direction=left><img src="images/know before buy.png" alt="" width="415" height="52" border="0"></marquee></td>
+								<td width="711" class="menu">
+				                        
+								</td>
+							  </tr>
+							</table>
+							<table width="100%"  border="0" cellspacing="0" cellpadding="0">
+								<tr>
+								  <td width="60%" height="66" align="left" valign="top">
+										  <table width="597"  border="0" cellspacing="0" cellpadding="0">
+											<tr>
+											  <td><img src="images/afterlogo.jpg" width="1000" height="38"></td>
+											  <td width="58"><a href="index.html"><img src="images/home.png" alt="" width="47" height="48" border="0"></a></td>
+											</tr>
+											
+										  </table>
+								  </td>
+								</tr>
+							</table>
+							<!-- start header-->
+							<table width="996"  border="0" cellspacing="0" cellpadding="0">
+								<tr>
+								  <td><img src="images/header.jpg" alt="" width="996" height="300" border="0"></td>
+								  
+								</tr>
+							</table>
+							<!-- end header -->
+						<table width="996"  border="0" cellspacing="0" cellpadding="0">
+							<tr align="left" valign="top">
+									  <td width="202" height="334">
+												  <table width="202"  border="0" cellspacing="0" cellpadding="0">
+													<tr>
+													  <td height="320" align="left" valign="top">
+														<div class="category"><img src="images/spacer.gif" alt="" width="30" height="19">Categories<img src="images/spacer.gif" alt="" width="10" height="10" border="0"></a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="laptop.html">Laptop</a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="mouse.html">Mouse</a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="keyboard.html">Key Board</a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="harddisk.html">Hard Disk</a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="ram.html">RAM</a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="pendrive.html">Pen Drive</a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="headphone.html">Head Phone</a></div>
+														<div class="linkmenu"><img src="images/spacer.gif" alt="" width="20" height="19"><span class="red">&bull;</span>&nbsp;&nbsp;<a href="bag.html">Bags </a></div>
+														
+													</tr>
+													<tr>
+													  <td height="26">&nbsp;</td>
+													</tr>
+													<tr>
+													  <td align="center"><marquee direction=up>
+													  <img src="images/banner.jpg" alt="" width="197" height="215" border="0">
+													  <img src="images/mob2.jpg" alt="" width="197" height="215" border="0">
+													  <img src="images/mob1.jpg" alt="" width="197" height="215" border="0">
+													  </marquee></td>
+													</tr>
+												  </table>
+									  </td>
+									  <td width="20">&nbsp;</td>
+									  <td width="774">
+											 
+													<table width="774"  border="0" cellspacing="0" cellpadding="0">
+										
+																
+																<tr>
+																  <td height="40" class="popular"><img src="images/spacer.gif" alt="" width="27" height="19"><span class="popular_text">Popular Models</span></td>
+																</tr>
+																	  <td height="138" valign="top" class="popular_center">
+																			  <table width="774" height="139"  border="0" cellpadding="0" cellspacing="0">
+																				<tr>
+																				  <td width="15">&nbsp;</td>
+																				  <td width="743"><table width="100%"  border="0" cellspacing="0" cellpadding="0">
+																					<tr>
+																					  <td width="21%" height="148" align="center"><a href="mouse.html"><img src="images/mouse1.jpg" alt="" width="140" height="117" border="0"></a><br>
+																						<span class="style7">Mouse</span></td>
+																					  <td width="2%" align="center"><img src="images/op.jpg" width="14" height="117"></td>
+																					  <td width="28%" align="center"><a href="mouse.html"><img src="images/mouse2.jpg" alt="" width="194" height="117" border="0"></a><br>
+																						<span class="style7">Mouse</span></td>
+																					  <td width="2%" align="center"><img src="images/op.jpg" width="14" height="117"></td>
+																					  <td width="22%" align="center"><a href="mouse.html"><img src="images/mouse3.jpg" alt="" width="165" height="117" border="0"></a><br>
+																						<span class="style7">Mouse</span></td>
+																					  <td width="1%"><img src="images/op.jpg" width="14" height="117"></td>
+																					  <td width="24%" align="center"><a href="mouse.html"><img src="images/mouse6.jpg" alt="" width="178" height="117" border="0"></a>
+																					  <span class="style7">
+																					Mouse</span></td>
+																					</tr>
+																				  </table></td>
+																				  <td width="16">&nbsp;</td>
+																				</tr>
+																			  </table>
+																	  </td>
+																</tr>
+																<tr>
+																  <td valign="top"><img src="images/close.gif" alt="" width="774" height="9"></td>
+																</tr>
+													</table>
+											  
+											  
+											  
+													<table width="774"  border="0" cellspacing="0" cellpadding="0">
+														  <tr>
+																<td width="22">&nbsp;</td>
+																<td width="729">&nbsp;</td>
+																<td width="23">&nbsp;</td>
+														  </tr>
+														  
+														  <tr>
+																<td height="479">&nbsp;</td>
+																<td align="left" valign="top">
+															<h3>
+																					<?php
+																							$cost = $_GET['cost'];
+																							$type = $_GET['type'];
+																																														
+																							$server = "localhost";
+																							$login = "root";
+																							$password = "root";
+																							$link = mysql_connect($server, $login, $password);
+																							if (!$link) {
+																								die("\n<BR>Could not connect $server:" .  mysql_error());
+																							}
+																							$db_selected = mysql_select_db("test");
+																							if (!$db_selected) {
+																								die ("\n<BR>Can\'t use Table : $db_selected" . 	mysql_error());
+																							}
+																							$result=mysql_query("SELECT *FROM mouse where mtype='$type' and mcost<='$cost'");
+																							?>
+                                                                                                                                                                                        <table border="5">
+																							<tr border="5"><th>Company</th><th>Model No</th><th>Type</th><th>Cost</th><th>Image</th></tr>
+																							<?php
+																							While($row=mysql_fetch_assoc($result))
+																							{
+																							$ii=$row['img_path'];?>
+																							<tr><td><?php echo $row['mCompany'];?></td>
+																							<td><?php echo $row['mmodel'];?></td>
+																							<td><?php echo $row['mtype'];?></td>
+																							<td><?php echo $row['mcost'];?></td>
+                                                                                                                                                                                        <td><?php echo "<img src='$ii' width='100' height='100'/>";?></td></tr>																			
+																							<?php
+																							}
+																							if(!$row)
+																							{
+                                                                                                                                                                                            ?>
+                                                                                                                                                                                        }
+																							<tr><td><?php echo "No more results  Found"?></td></tr><?php
+																							}
+																							?>
+																							</table><br><br><br>
+																						
+																</td></h3>
+																<td>&nbsp;</td>
+														  </tr>
+													</table>
+										</td>
+							</tr>
+						  </table>
+						  <table width="996"  border="0" cellspacing="0" cellpadding="0">
+							<tr>
+							  <td><img src="images/footer.jpg" alt="" width="996" height="5"></td>
+							</tr>
+							
+							  </table></td>
+							</tr>
+						  </table>
+				  
+				</td>
+			</tr>
+		</table>
+</form>
+</body>
+</html>
